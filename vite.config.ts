ee369180e357d87197a9905b80bf40c6e910a047
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2025-12-15 02:00:23 +0900

    fix:rootの再設定

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// リポジトリ名を取得するか、ハードコードで設定します。
-// 例: https://username.github.io/my-app/ の場合、ベースパスは '/my-app/'
-const repoName = 'TKmatsu2008'; // 例: 'my-app'
-
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, 
+  base: `/TKmatsu2008/`, 
 })
