f827db91b8e1f2eeba44b3d3fe6c9d14b7184787
TKmatsu2008 <fhenixeaglu1st@icloud.com>
2025-12-15 13:58:55 +0900

    feat:ポートフォリオの雛形を作成。まだ詳しい中身はない

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
 import './App.css'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 function App() {
   const navLinks = [
@@ -10,11 +19,11 @@ function App() {
   ]
 
   return (
-    <div className="">
-      <header className ="sticky top-0 bg-white shadow-md">
-        <div className='container mx-auto flex flex-row flex-nowrap items-center justify-start px-4 py-2 space-x-6'>
+    <div className="w-full min-h-full transition-all duration-300">
+      <header className ="w-full sticky top-0 bg-gray-100">
+        <div className='w-full max-w-full md:max-w-7xl md:mx-auto mx-auto flex flex-row flex-nowrap items-center justify-between px-4 py-2'>
           <a href="/" className='flex items-center space-x-2 flex-shrink-0'>
-            <img src="src/assets/logo.png" alt="Logo" className="header-logo object-contain text-orange-500" />
+            <img src="src/assets/logo.png" alt="Logo" className="header-logo h-4 w-4 md:h-5 md:w-5 lg:h-6 lg:w-6 object-contain text-orange-500" />
           </a>
           <nav className='flex flex-row flex-nowrap items-center space-x-8'>
             {navLinks.map((link)=>(
@@ -29,20 +38,42 @@ function App() {
         </div>
       </header>
 
-      <main className='min-h-screen p-4'>
-        <section id='profile' className='min-h-screen'>
+      <main className='w-full p-4 pb-20'>
+        <section id='profile' className='min-h-screen md:max-w-3xl md:mx-auto md:pt-[100px]'>
           <h1 className='text-gray-600 leading-relaxed'>
             Profile
           </h1>
-          <p className='text-gray-600 leading-relaxed'>
-            こんにちわ。TKmatsu2008です。 --- I am TKmatsu2008.
+          <p className='text-gray-600 leading-relaxed min-h-full'>
+            髙松一鷹
+            <br /><br />
+            筑波大学大学院 システム情報工学研究群 情報理工学位プログラム 修士課程2年（在籍中）
+            <br /><br />
+            2020-2024 筑波大学 情報学群 情報メディア創成学類
+            <br />
+            2024-現在 筑波大学大学院 システム情報工学研究群 情報理工学位プログラム
+            <br /><br />
+            チーム開発が非常に好きで、チーム開発を通じてハッカソンや、LLMを利用したプロダクト、組込み開発など様々なことに挑戦してきました。
+            最近は自分自身の技術力を向上させることにハマっており、esportsのダッシュボードなどを作っていました。デプロイはまだまだ赤子です。
           </p>
         </section>
 
-        <section id='project' className='min-h-screen'>
+        <section id='project' className='w-full max-w-full md:max-w-7xl md:mx-auto mx-auto min-h-screen'>
           <h1 className='text-gray-600 leading-relaxed'>
             Project
           </h1>
+          <Card className="border border-gray-300 shadow-lg bg-white">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
         </section>
 
         <section id='research' className='min-h-screen'>
@@ -58,12 +89,14 @@ function App() {
         </section>
       </main>
 
-      <footer className='sticky bottom-0 bg-white shadow-md'>
-        <section id='contact' className='min-h-screen'>
-          <h2 className='text-gray-600 leading-relaxed'>
-            Contact
-          </h2>
-        </section>
+      <footer className='bg-white bg-gray-100'>
+        <div className='w-full md:max-w-5xl md:mx-auto'>
+          <section id='contact' className=''>
+            <h2 className='text-gray-600 leading-relaxed'>
+              Contact
+            </h2>
+          </section>
+        </div>
       </footer>
     </div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 // vite.config.js
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: `/TKmatsu2008/`, 
+    plugins: [react(), tailwindcss()],
+    base: `/TKmatsu2008/`, 
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
 })
